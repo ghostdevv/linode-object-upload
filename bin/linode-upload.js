381092233dea097b3095d34f36941f6b41d9c592
GHOST <ghostdevbusiness@gmail.com>
2021-01-03 19:26:55 +0000

    feat: updated script

--- bin/linode-upload.js
+++ bin/linode-upload.js
@@ -4,36 +4,41 @@ require('dotenv').config();
 const aws = require('aws-sdk');
 const dash = require('dashargs');
 const path = require('path');
+const joinURL = require('url-join');
 const { readFile } = require('fs');
 
 const args = dash.argv();
-const str = dash.strip(args.string);
 
-const { bucket, key, secret, endpoint } = args;
-const file = str.split(' ')[str.split(' ').length - 1];
+const { key, secret } = args;
+const file = process.argv[2];
+const bucket = process.argv[3];
+const region = process.argv[4];
 
 if (!bucket) return console.log('Please provide a bucket | -bucket bucket-name');
 if (!key) return console.log('Please provide your access key | -key key');
 if (!secret) return console.log('Please provide your access secret | -secret secret');
-if (!endpoint) return console.log('Please provide your endpoint | -endpoint https://us-east-1.linodeobjects.com');
+if (!region) return console.log('Please provide your region | -region us-east-1');
 if (!file) return console.log('Please provide a file path');
 
-const key = path.basename(file);
+const fileName = path.basename(file);
 
 readFile(path.join(process.cwd(), file), {}, (error, data) => {
     if (error) return console.log('There was a error reading your file\n' + error);
 
     const client = new aws.S3({
-        accessKeyId: process.env.ACCESS_ID,
-        secretAccessKey: process.env.ACCESS_SECRET,
-        endpoint: new aws.Endpoint(process.env.ENDPOINT),
+        accessKeyId: key,
+        secretAccessKey: secret ,
+        endpoint: new aws.Endpoint(joinURL(
+            'https://',
+            region + '.linodeobjects.com'
+        )),
     });
 
     console.log('Uploading...');
     
     client.putObject({
         Bucket: bucket,
-        Key: key,
+        Key: fileName,
         Body: data
     })
     .promise()
