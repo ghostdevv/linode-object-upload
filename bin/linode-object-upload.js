1ac5a4fa4d39041e4dd30ae2c7dc1866f049d44e
GHOST <ghostdevbusiness@gmail.com>
2021-06-08 17:10:41 +0100

    format: run prettier

--- bin/linode-object-upload.js
+++ bin/linode-object-upload.js
@@ -14,34 +14,40 @@ const file = process.argv[2];
 const bucket = process.argv[3];
 const region = process.argv[4];
 
-if (!bucket) return console.log('Please provide a bucket | -bucket bucket-name');
+if (!bucket)
+    return console.log('Please provide a bucket | -bucket bucket-name');
 if (!key) return console.log('Please provide your access key | -key key');
-if (!secret) return console.log('Please provide your access secret | -secret secret');
-if (!region) return console.log('Please provide your region | -region us-east-1');
+if (!secret)
+    return console.log('Please provide your access secret | -secret secret');
+if (!region)
+    return console.log('Please provide your region | -region us-east-1');
 if (!file) return console.log('Please provide a file path');
 
 const fileName = path.basename(file);
 
 readFile(path.resolve(file), {}, (error, data) => {
-    if (error) return console.log('There was a error reading your file\n' + error);
+    if (error)
+        return console.log('There was a error reading your file\n' + error);
 
     const client = new aws.S3({
         accessKeyId: key,
-        secretAccessKey: secret ,
-        endpoint: new aws.Endpoint(joinURL(
-            'https://',
-            region + '.linodeobjects.com'
-        )),
+        secretAccessKey: secret,
+        endpoint: new aws.Endpoint(
+            joinURL('https://', region + '.linodeobjects.com'),
+        ),
     });
 
     console.log('Uploading...');
-    
-    client.putObject({
-        Bucket: bucket,
-        Key: fileName,
-        Body: data
-    })
-    .promise()
-    .then(() => console.log('Uploaded'))
-    .catch(e => (console.error('Error in uploading\n' + e), process.exit(1)));
+
+    client
+        .putObject({
+            Bucket: bucket,
+            Key: fileName,
+            Body: data,
+        })
+        .promise()
+        .then(() => console.log('Uploaded'))
+        .catch(
+            (e) => (console.error('Error in uploading\n' + e), process.exit(1)),
+        );
 });
